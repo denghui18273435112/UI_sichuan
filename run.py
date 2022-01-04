a355c6dd88f3ffe50d4c0c068e200490aea02a93
denghui18273435112 <314983713@qq.com>
2022-01-04 23:31:41 +0800

    Merge branch 'master' of https://github.com/denghui18273435112/UI_sichuan
    # Conflicts:
    #	run.py

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     os.system("allure generate {0} -o {1} --clean".format(result_path, allure_report_path))
     # os.system("allure serve {}".format(result_path))
     #公司提交
-
+    #家里提交
